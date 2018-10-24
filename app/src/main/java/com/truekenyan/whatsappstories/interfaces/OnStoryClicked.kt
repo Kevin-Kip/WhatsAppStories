3a6352740ac56a71acb3fc6842ba55bd3ff3f18c
Kevin-Kip <masterfork5@gmail.com>
2018-10-24 22:05:30 +0300

    More changes

---
+++ app/src/main/java/com/truekenyan/whatsappstories/interfaces/OnStoryClicked.kt
@@ -0,0 +1,7 @@
+package com.truekenyan.whatsappstories.interfaces
+
+interface OnStoryClicked {
+    fun onSaveButtonClicked(path: String)
+    fun onViewButtonClicked()
+    fun onImageClicked()
+}
