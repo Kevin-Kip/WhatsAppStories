e674a895bc8b8940c7cc25d2f8188c034d44509c
Kevin-Kip <masterfork5@gmail.com>
2018-10-28 15:44:54 +0300

    Fixed issues

--- app/src/main/java/com/truekenyan/whatsappstories/adapters/StoryAdapter.kt
+++ app/src/main/java/com/truekenyan/whatsappstories/adapters/StoryAdapter.kt
@@ -1,6 +1,5 @@
 package com.truekenyan.whatsappstories.adapters
 
-import android.content.Context
 import android.graphics.drawable.Drawable
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
@@ -26,31 +25,31 @@ class StoryAdapter(private var storyList: List<Story>) : RecyclerView.Adapter<St
     override fun getItemCount(): Int = storyList.size
 
     override fun onBindViewHolder(holder: StoryViewHolder, position: Int) {
-        val currentStory: Story = storyList[holder.adapterPosition]
         holder.bind(storyList[position])
-
-        holder.downloadButton.setOnClickListener(clickListener(Commons.SAVE, currentStory.path))
-        holder.viewButton.setOnClickListener(clickListener(Commons.VIEW, currentStory.path))
-    }
-
-    private fun clickListener(action: String, path: String): View.OnClickListener {
-        return View.OnClickListener {
-            if (action == Commons.VIEW) {
-                //TODO view
-            } else if (action == Commons.SAVE) {
-                //TODO save
-            }
-        }
     }
 
     class StoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         private val storyImage: ImageView = itemView.findViewById(R.id.status_image)!!
-        val downloadButton: ImageView = itemView.findViewById(R.id.button_download)!!
-        val viewButton: ImageView = itemView.findViewById(R.id.button_view)!!
+        private val downloadButton: ImageView = itemView.findViewById(R.id.button_download)!!
+        private val viewButton: ImageView = itemView.findViewById(R.id.button_view)!!
 
         fun bind(story: Story){
             if (story.type == Type.Photo) {
                 storyImage.setImageDrawable(Drawable.createFromPath(story.path))
+            }
+
+            storyImage.setOnClickListener(clickListener(Commons.VIEW, story.path))
+            downloadButton.setOnClickListener(clickListener(Commons.SAVE, story.path))
+            viewButton.setOnClickListener(clickListener(Commons.VIEW, story.path))
+        }
+
+        private fun clickListener(action: String, path: String): View.OnClickListener {
+            return View.OnClickListener {
+                if (action == Commons.VIEW) {
+                    //TODO view
+                } else if (action == Commons.SAVE) {
+                    //TODO save
+                }
             }
         }
     }
