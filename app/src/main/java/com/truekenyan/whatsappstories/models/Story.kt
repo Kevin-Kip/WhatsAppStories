312b6cb02f28ba4df0d7302b8458b881ab55ec11
Kevin-Kip <masterfork5@gmail.com>
2018-10-28 14:52:19 +0300

    Fixed issues

--- app/src/main/java/com/truekenyan/whatsappstories/models/Story.kt
+++ app/src/main/java/com/truekenyan/whatsappstories/models/Story.kt
@@ -1,6 +1,5 @@
 package com.truekenyan.whatsappstories.models
 
-import com.truekenyan.whatsappstories.utilities.Commons
 import java.io.Serializable
 
 data class Story(var type: Enum<Type> , var path: String) : Serializable
