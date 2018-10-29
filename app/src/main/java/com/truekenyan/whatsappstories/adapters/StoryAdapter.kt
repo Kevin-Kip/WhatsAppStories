7af75c4a7cb48ed756a598f7824882b3308b60ea
Kevin-Kip <masterfork5@gmail.com>
2018-10-29 18:44:20 +0300

    Shuffling around recyclerview initialization in an attempt to fix the 'no adapter error'

--- app/src/main/java/com/truekenyan/whatsappstories/adapters/StoryAdapter.kt
+++ app/src/main/java/com/truekenyan/whatsappstories/adapters/StoryAdapter.kt
@@ -1,5 +1,6 @@
 package com.truekenyan.whatsappstories.adapters
 
+import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -8,14 +9,14 @@ import com.truekenyan.whatsappstories.R
 import com.truekenyan.whatsappstories.holders.StoryViewHolder
 import com.truekenyan.whatsappstories.models.Story
 
-class StoryAdapter(private var storyList: List<Story>) : RecyclerView.Adapter<StoryViewHolder>() {
+class StoryAdapter(private var storyList , private val context: Context) : RecyclerView.Adapter<StoryViewHolder>() {
 
     init {
         storyList = mutableListOf()
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, p1: Int): StoryViewHolder {
-        val rootView: View = LayoutInflater.from(parent.context).inflate(R.layout.item_story, parent,false)
+        val rootView: View = LayoutInflater.from(context).inflate(R.layout.item_story, parent,false)
         return StoryViewHolder(rootView)
     }
 
--- app/src/main/java/com/truekenyan/whatsappstories/fragments/FragmentPhotos.kt
+++ app/src/main/java/com/truekenyan/whatsappstories/fragments/FragmentPhotos.kt
@@ -1,5 +1,6 @@
 package com.truekenyan.whatsappstories.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
@@ -8,6 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import com.truekenyan.whatsappstories.R
 import com.truekenyan.whatsappstories.activities.MainActivity
 import com.truekenyan.whatsappstories.adapters.StoryAdapter
@@ -20,56 +22,54 @@ class FragmentPhotos: Fragment() {
     private lateinit var photosRecycler: RecyclerView
     private lateinit var emptyTextView: TextView
 
-    private val adapter = StoryAdapter(photos)
+    private lateinit var adapter: StoryAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView: View = inflater.inflate(R.layout.fragment_photos, container, false)
 
         photosRecycler = rootView.findViewById(R.id.photos_list)
         emptyTextView = rootView.findViewById(R.id.empty_text_view)
-
-        photosRecycler.adapter = StoryAdapter(photos)
-        photosRecycler.layoutManager = LinearLayoutManager(context)
-        photosRecycler.setHasFixedSize(true)
+        adapter = StoryAdapter(photos, context as Context)
 
         photos.clear()
-
-        emptyTextView.apply {
-            visibility = View.VISIBLE
-            textSize = 10F
-        }
 
         for (item in MainActivity.getStories()) {
             if (item.type == Type.Photo) {
                 photos.add(item)
-                emptyTextView.append("\n ${item.path}")
             }
         }
-
         adapter.notifyDataSetChanged()
 
-//        if (photos.size == 0) {
-//            photosRecycler.visibility = View.GONE
-//            emptyTextView.visibility = View.VISIBLE
-//        } else {
-//            photosRecycler.visibility = View.VISIBLE
-//            emptyTextView.visibility = View.GONE
-//            photosRecycler.apply {
-//                adapter = StoryAdapter(photos)
-//                hasFixedSize()
-//                layoutManager = if(context.resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
-//                    GridLayoutManager(context, 2)
-//                } else {
-//                    GridLayoutManager(context, 4)
-//                }
-//                layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
-//            }
-//        }
+        photosRecycler.apply {
+            adapter = adapter
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+        }
+
+        Toast.makeText(context, "${adapter.itemCount} items in adapter: onCreateView", Toast.LENGTH_SHORT).show()
 
         return rootView
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+//        photos.clear()
+//
+//        for (item in MainActivity.getStories()) {
+//            if (item.type == Type.Photo) {
+//                photos.add(item)
+//            }
+//        }
+
+//        photosRecycler.apply {
+//            adapter = adapter
+//            layoutManager = LinearLayoutManager(context)
+//            setHasFixedSize(true)
+//        }
+
+//        adapter.notifyDataSetChanged()
+
+        Toast.makeText(context, "${adapter.itemCount} items in adapter", Toast.LENGTH_SHORT).show()
     }
 }
