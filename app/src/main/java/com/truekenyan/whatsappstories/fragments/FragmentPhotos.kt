38547611986f9d3582e527f860e501e1dc538ef1
Kevin-Kip <masterfork5@gmail.com>
2018-10-29 11:32:51 +0300

    Fixed issues

--- app/src/main/java/com/truekenyan/whatsappstories/fragments/FragmentPhotos.kt
+++ app/src/main/java/com/truekenyan/whatsappstories/fragments/FragmentPhotos.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.support.v7.widget.StaggeredGridLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
